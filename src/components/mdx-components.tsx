9a77e3e63bd4a72eeed2b300d33100c679d42906
kodiyak <mathewsto51@gmail.com>
2026-09-04 15:11:02 -0300

    feat: update MDX components for improved styling; add new notes and manifest files

--- src/components/mdx-components.tsx
+++ src/components/mdx-components.tsx
@@ -1,6 +1,7 @@
 import type { MDXComponents } from "mdx/types";
 import Image, { type ImageProps } from "next/image";
 import Link from "next/link";
+import { cn } from "@/lib/utils";
 import { Separator } from "./ui/separator";
 
 export function getMDXComponents(components: MDXComponents): MDXComponents {
@@ -17,14 +18,23 @@ export function getMDXComponents(components: MDXComponents): MDXComponents {
         {...(props as ImageProps)}
       />
     ),
-    h1: (props) => <h1 className="text-xl mb-2 font-bold" {...props} />,
-    h2: (props) => <h2 className="text-lg mb-2 font-bold" {...props} />,
-    h3: (props) => <h3 className="text-base mb-2 font-bold" {...props} />,
-    p: (props) => <p className="min-h-6 text-sm" {...props} />,
+    h1: (props) => <h1 className="text-base mt-6 mb-2 font-bold" {...props} />,
+    h2: (props) => <h2 className="text-base mt-5 mb-2 font-bold" {...props} />,
+    h3: (props) => <h3 className="text-base mt-4 mb-2 font-bold" {...props} />,
+    p: (props) => <p className="min-h-6 text-xs" {...props} />,
+    li: (props) => (
+      <li className={cn("text-sm ", "list-disc list-inside")} {...props} />
+    ),
     hr: (props) => (
       <div className="px-8 py-6">
         <Separator {...props} />
       </div>
+    ),
+    blockquote: (props) => (
+      <blockquote
+        className="border-l-2 text-muted-foreground pl-4 text-sm mb-4 italic"
+        {...props}
+      />
     ),
     ...components,
   };
